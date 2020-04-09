bb8776bdea19c214466c17fc3407cec63a616a4d
eisuke-hayashi <email>
2020-04-09 17:27:05 +0900

    【ポートフォリオ実装】20200409

--- src/App.vue
+++ src/App.vue
@@ -6,36 +6,32 @@
    <Skill />
    <Vision />
    <Footer />
+   <Menu />
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
-// import Main from './components/Main.vue'
+import Main from './components/Main.vue'
 // import About from './components/About.vue'
 // import Skill from './components/Skill.vue'
 // import Vision from './components/Vision.vue'
 // import Footer from './components/Footer.vue'
+//import Menu from './components/Footer.vue'
 export default {
   name: 'App' , 
   components: {
     Header,
-    // Main,
+    Main,
     // About,
     // Skill,
     // Vision,
     // Footer,
+   // Menu,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,44 +1,49 @@
 <template>
-  <div class="Main">
-   <h1>aa</h1>
-    
+  <div id="mainSection">
+   <div id="userName">
+     E.Hayashi
+   </div>
+   <div id="companyName">
+     Seattleconsulting
+   </div>
+   <img id="profilePicture" src="../assets/img/test2.png">
   </div>
 </template>
 
 <script>
-  import Header from './components/Header.vue'
-  import Main from './components/Main.vue'
-  import About from './components/About.vue'
-  import Skill from './components/Skill.vue'
-  import Vision from './components/Vision.vue'
-  import Footer from './components/Footer.vue'
-  export default {
-    name: 'App' , 
-    components: {
-      Header,
-      Main,
-      About,
-      Skill,
-      Vision,
-      Footer,
+export default {
+    name: 'Main' ,
+    props:{
+      msg:String
+    }
 }
-  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+#mainSection{
+  background: linear-gradient(#80E1F7,#06C1E6);
+  width: 100%;
+  height: auto;
+  text-align: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+#userName{
+  color: #FFFFFF;
+  font-family:"Noto Sans";
+  font-size: 18pt;
+  font-weight: bold;
+  text-shadow:2px 3px 9px #191314;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+#companyName{
+  color: #FFFFFF;
+  font-family:"Noto Sans";
+  font-size: 18pt;
+  font-weight: bold;
+  text-shadow:2px 3px 9px #191314;
 }
-a {
-  color: #42b983;
+#profilePicture{
+  border-radius: 50%;
+  width: 64px;
+  height: 64px;
 }
 </style>
