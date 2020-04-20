dba8a848f0a3b82ff50f6b716066e24db646da73
eisuke-hayashi <email>
2020-04-20 18:17:06 +0900

    【ポートフォリオ実装】20200420

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import './assets/css/reset.css'
 import smoothScroll from 'vue-smoothscroll'
+import axios from 'axios'
+Vue.use(axios)
 Vue.use(smoothScroll)
 Vue.config.productionTip = false
 
