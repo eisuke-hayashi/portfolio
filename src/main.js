20b4688c6e805c284047b8174699baf59027b756
eisuke-hayashi <email>
2020-04-21 12:02:23 +0900

    branch chenge

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import './assets/css/reset.css'
 import smoothScroll from 'vue-smoothscroll'
+import store from './store'
 import axios from 'axios'
 Vue.use(axios)
 Vue.use(smoothScroll)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  store,
+  components: { App },
+  template: '<App/>'
+})
--- src/store.js
+++ src/store.js
@@ -1,23 +1,21 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
 
-  //state:コンポーネントでいうdata
+  //state:コンポーネントでいうdata/アプリケーションの状態（情報）
   state: {
     message: '初期メッセージ'
   },
 
-  //getters:コンポーネントでいうcomputed的なもの
+  //getters:コンポーネントでいうcomputed的なもの/stateの一部やstateから返された値を保持する
   getters:{
     //messageを使用するgetter
     message(state) {
       return state.message
     }
   },
-
   //mutations:コンポーネントでいうmethod（と言うかsetter）
   //stateを唯一変更できるもの
   mutations: {
@@ -27,7 +25,6 @@ const store = new Vuex.Store({
       state.message = payload.message
     }
   },
-
   //actionのコミットを使うことでミューテーションを呼び出す（コンポーネントには無い概念）
   actions: {
     doUpdate({commit}, message){
